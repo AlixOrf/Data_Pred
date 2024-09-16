1feda30b6e57de05177952a198ba60c7a9f59293
Kiaraw <kiara.wurtz@epitech.digital>
2024-09-16 20:10:01 +0200

    Update Nettoyage.py

--- Nettoyage.py
+++ Nettoyage.py
@@ -10,3 +10,13 @@ def trier_weather():
     return weather_filtered
 
 print(trier_weather())
+
+def trier_csv():
+    # Change les noms des colonnes stp
+    csv_colonnes = ['apply_time_rl', 'fact_time', 'gfs_2m_dewpoint', 'gfs_total_clouds_cover_low', 'gfs_wind_speed', 'gfs_u_wind', 'gfs_v_wind']
+    circuits_csv = pd.read_csv('Ressources/circuits.csv', usecols=csv_colonnes)
+    circuits_csv.to_csv('Ressources/circuits_filtered.csv', index=False)
+    circuits_filtered = pd.read_csv('Ressources/circuits_filtered.csv')
+    return circuits_filtered
+
+print(trier_csv())
