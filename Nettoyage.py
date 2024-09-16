4ac3834fbc2d1c5ac5b47e67e1a6c736c8becbad
AlixOrf <alix.orfeuvre@epitech.digital>
2024-09-16 19:51:32 +0200

    Update Nettoyage.py

--- Nettoyage.py
+++ Nettoyage.py
@@ -1,12 +1,12 @@
 import pandas as pd 
 import pyarrow as py
+import pyarrow.parquet as pq
 
-important_colonnes = ['date', 'name']
+def trier_weather():
+    important_colonnes = ['apply_time_rl', 'fact_time','gfs_2m_dewpoint','gfs_total_clouds_cover_low', 'gfs_wind_speed','gfs_u_wind','gfs_v_wind']
+    weather = pd.read_parquet('Ressources/weather.parquet',columns=important_colonnes)
+    weather.to_parquet('Ressources/weather_filtered.parquet')
+    weather_filtered = pd.read_parquet('Ressources_filtrer/weather_filtered.parquet')
+    return weather_filtered
 
-df_parquet = pd.read_parquet('Ressources/weather.parquet',columns=important_colonnes)
-
-df_parquet.to_parquet('Ressources/weather_filtered.parquet')
-
-df_parquet_filtered = pd.read_parquet('Ressources/weather_filtered.parquet')
-
-print(df_parquet_filtered)
+print(trier_weather())
