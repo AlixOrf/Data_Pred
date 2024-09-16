acba12d093aa88106c02952041e05d63232aae6d
Kiaraw <kiara.wurtz@epitech.digital>
2024-09-16 19:08:42 +0200

    Update Nettoyage.py

--- Nettoyage.py
+++ Nettoyage.py
@@ -1,6 +1,12 @@
 import pandas as pd 
 import pyarrow as py
 
-df_parquet = pd.read_parquet('Ressources/weather.parquet')
+important_colonnes = ['date', 'name']
 
-print(df_parquet.columns)
+df_parquet = pd.read_parquet('Ressources/weather.parquet',columns=important_colonnes)
+
+df_parquet.to_parquet('Ressources/weather_filtered.parquet')
+
+df_parquet_filtered = pd.read_parquet('Ressources/weather_filtered.parquet')
+
+print(df_parquet_filtered)
