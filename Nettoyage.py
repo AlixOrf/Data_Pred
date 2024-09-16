670f5f01757983adc9ee773b791ed9c6b3bc0ffa
Kiaraw <kiara.wurtz@epitech.digital>
2024-09-16 16:51:24 +0200

    Update Nettoyage.py

--- Nettoyage.py
+++ Nettoyage.py
@@ -1,4 +1,6 @@
 import pandas as pd 
 import pyarrow as py
 
-df_parquet = pd.read_parquet('../Ressources/nom_du_fichier.parquet')
+df_parquet = pd.read_parquet('Ressources/weather.parquet')
+
+print(df_parquet)
